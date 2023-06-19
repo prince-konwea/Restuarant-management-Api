66c0ad10b09c574c230b0f1fd5a47e5d26d44fd4
prince-konwea <kokodecontee123@gmail.com>
2023-06-19 05:25:09 -0700

    creating invoice routes

---
+++ routes/invoiceRouter.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controller "github.com/prince-konwea/restuarant-management-api/controllers"
+)
+
+func InvoiceRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/invoice", controller.GetInvoices())
+	incomingRoutes.GET("/invoice/:invoice_id", controller.GetInvoice())
+	incomingRoutes.POST("/invoices", controller.CreateInvoice())
+	incomingRoutes.PATCH("/invoices/invoice_id", controller.UpdateInvoice())
+}
