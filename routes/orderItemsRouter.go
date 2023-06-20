125271cdb6435337011d0e5bbcc061eab8dd0c00
prince-konwea <kokodecontee123@gmail.com>
2023-06-20 05:34:51 +0100

    creating order items controller function outline

--- routes/orderItemsRouter.go
+++ routes/orderItemsRouter.go
@@ -8,7 +8,7 @@ import (
 func OrderItemRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/orderItems", controller.GetOrderItems())
 	incomingRoutes.GET("/orderItems/:orderItem_id", controller.GetOrderItem())
-	incomingRoutes.GET("/orderItems-order/:order_id", controller.GetOrderItemByOrder())
+	incomingRoutes.GET("/orderItems-order/:order_id", controller.GetOrderItemsByOrder())
 	incomingRoutes.POST("/orderItem", controller.CreateOrderItem())
 	incomingRoutes.PATCH("/orderItem/orderItem_id", controller.UpdateOrderItem())
 }
